5329fe78397f71af8851225f2d8f2b4ee09781f6
Chase_55 <14172331+chase55@user.noreply.gitee.com>
2025-05-16 13:29:03 +0800

    refactor: 项目依赖变动

--- src/main/java/top/yixuanoct/whut_webapp_server/exception/InvalidCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package top.yixuanoct.whut_webapp_server.exception;
-
-public class InvalidCredentialsException extends RuntimeException {
-    public InvalidCredentialsException() {
-        super();
-    }
-}
--- src/test/java/cn/edu/whut/sept/zuul/ZuulApplicationTests.java
+++ src/test/java/cn/edu/whut/sept/zuul/ZuulApplicationTests.java
@@ -1,13 +1,13 @@
-package top.yixuanoct.whut_webapp_server;
+package cn.edu.whut.sept.zuul;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WhutWebappServerApplicationTests {
+class ZuulApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
